2d21635f98b98221cf167667b01f67bc913f1246
francisco <iamfranciscobtw@gmail.com>
2024-04-19 17:24:46 -0500

    se creo la clase testeando para intentar resolver un error

--- src/main/java/co/com/sofkau/Main.java
+++ src/main/java/co/com/sofkau/Main.java
@@ -1,10 +1,11 @@
-package co.com.sofkaU;
+package co.com.sofkau;
+
+
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-        
     }
 }
